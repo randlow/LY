046eed56c430d6b2d0de4704e55c58d9a7d3f4d3
lexie <curtaincall1025@gmail.com>
2014-09-17 12:45:09 +1000

    contains all crawlers using scrapy framework

---
+++ crawlers/returnData/spiders/goldSpider.py
@@ -0,0 +1,19 @@
+import scrapy
+import json
+from returnData.items import ReturndataItem
+
+class GoldSpider(scrapy.Spider):
+	name = "gold"
+	allowed_domains = ["www.bloomberg.com"]
+	start_urls = ["http://www.bloomberg.com/markets/chart/data/1Y/XAUUSD:CUR"]
+
+	def parse(self, response):
+		data=json.loads(response.body_as_unicode())
+
+		item=ReturndataItem()
+
+		daily = data["data_values"]
+		
+		for d in daily:
+			item['closeprice']=d[1]
+			yield item
