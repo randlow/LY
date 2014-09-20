90eb4e88dd094efcde78464920a674d2e485afc0
lexie <curtaincall1025@gmail.com>
2014-09-20 20:54:10 +1000

    add date on x axis

--- plot/goldplot.py
+++ plot/goldplot.py
@@ -1,18 +1,41 @@
 import numpy as np
+import datetime
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
-
+from matplotlib import dates
 
 
 def graph():
 	path = r'../scraped/gold.csv'
-	price = np.genfromtxt(path, delimiter=',', unpack=True)
+	data = np.genfromtxt(path, delimiter=",", names=True)
+	price=data['closeprice']
+	epoch=data['epochtime']
+
+	#convert timestamp to date
+	temp=[]
+	for e in epoch:
+		s=e/1000
+		temp.append(datetime.datetime.fromtimestamp(s))
+
+	#print temp
+
+	fig = plt.figure("Gold return")
+	ax=fig.add_subplot(111)
+	ax.plot_date(temp, price, '.:', color='#245678')
+
+	xfmt=dates.DateFormatter('%d-%m-%Y')
+	ax.xaxis.set_major_formatter(xfmt)
 
 
-	plt.figure("Gold return")
-	plt.xlabel("")
+	#ax.xaxis.set_major_locator(dates.MonthLocator())
+	
+	plt.xlabel("Month")
 	plt.ylabel("Price(USD)")
-	plt.plot(price)
+
+	#plt.plot(price, temp, 'c.:')
+	plt.grid(True)
+
+	fig.autofmt_xdate()
 	plt.show()
 
 graph()
